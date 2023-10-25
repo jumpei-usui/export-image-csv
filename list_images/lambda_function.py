9eb302bde0210088070f6a411626b1be22c47995
Jumpei Usui <jumpeisoccerguitar@gmail.com>
2023-10-25 23:33:15 +0900

    develop environment

---
+++ list_images/lambda_function.py
@@ -0,0 +1,14 @@
+import os
+import json
+import boto3
+
+
+def handler(event, context):
+    bucket_name = os.environ.get("BUCKET_NAME")
+
+    s3_client = boto3.client("s3")
+    response = s3_client.list_objects_v2(Bucket=bucket_name, MaxKeys=100)
+
+    object_keys = [obj["Key"] for obj in response["Contents"]]
+
+    return json.dumps(object_keys)
---
+++ list_images/local_test.py
@@ -0,0 +1,5 @@
+from lambda_function import handler
+
+
+if __name__ == "__main__":
+    handler(event=None, context=None)
